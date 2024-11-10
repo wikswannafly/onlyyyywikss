51cd58d59996f25acce5eb19e11e6dbe2beb6e73
wikswannafly <dwitghcahhm@gmail.com>
2024-11-11 03:58:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function showAlert() {
-    window.location.href = "https://pornhub.com";  // Ganti dengan URL yang sesuai
+    window.location.href = "https://bloganda.com";  // Ganti dengan URL yang sesuai
 }
